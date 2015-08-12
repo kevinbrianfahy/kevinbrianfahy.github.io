2ed6c02872636ba5a437d42c1670323cdba076f3
kevinbrianfahy <kevinbrianfahy@gmail.com>
2015-08-11 22:51:53 -0500

    added jquery

--- effects.js
+++ effects.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
-  $('.video').on('mouseenter', function() {
+  $('video').on({mouseenter: function() {
     $('body').addClass('effects');
-    $('header').addClass('effects');
-    $('h1').addClass('name');
-  });
-  $('.video').on('mouseleave', function() {
+    $('.videotext').hide();
+    $('.curtaintop').show().animate({'top': '95px'}, 'fast');
+    $('.curtaintop').show().animate({'top': '45px'}, 'fast');
+    $('.curtaintop').show().animate({'top': '95px'}, 'fast');
+    $('.curtaintop').show().animate({'top': '65px'}, 'fast');
+    $('.curtaintop').show().animate({'top': '95px'}, 'fast');
+    $('.curtaintop').show().animate({'top': '85px'}, 'fast');
+    $('.curtaintop').show().animate({'top': '95px'}, 'fast');
+    $('.curtainleft').delay(1500).show().animate({'left': '180px'}, 2500);
+    $('.curtainright').delay(1500).show().animate({'right': '160px'}, 2500);
+    $('.seats').show();
+    },
+    mouseleave: function() {
     $('body').removeClass('effects');
-    $('header').removeClass('effects');
-    $('h1').removeClass('name');
+    $('.videotext').show();
+    $('.curtaintop').hide().animate({'top': '0px'});
+    $('.curtainleft').hide().animate({'left': '0px'});
+    $('.curtainright').hide().animate({'right': '0px'});
+    $('.seats').hide();
+    }
   });
 });
 
-$(document).ready(function() {
 
-  });
